e064bb44710f9b457af98251a11efffeb6e5b8eb
RayyanMFPP <rayyantelyu@gmail.com>
2026-03-30 03:33:28 +0700

    commit generic-class

--- TP_Modul5_103022400121/Program.cs
+++ TP_Modul5_103022400121/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + this.data);
+    }
+}
