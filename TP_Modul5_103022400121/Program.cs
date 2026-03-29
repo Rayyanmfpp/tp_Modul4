3a8cf2f635666ee26d06eec2b81e48d2e78a18dd
RayyanMFPP <rayyantelyu@gmail.com>
2026-03-30 03:45:09 +0700

    main sebelum merge

--- TP_Modul5_103022400121/Program.cs
+++ TP_Modul5_103022400121/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Rayyan");
+        DataGeneric<string> nimData = new DataGeneric<string>("103022400121");
+        nimData.PrintData();
+    }
+}
